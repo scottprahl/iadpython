424a8ce0149d3dffa4b30adca53394de11ef91c9
Scott Prahl <scott@prahls.com>
2021-06-06 15:39:50 -0700

    add cap calculation

--- iadpython/sphere.py
+++ iadpython/sphere.py
@@ -2,6 +2,8 @@
 # pylint: disable=too-many-instance-attributes
 # pylint: disable=too-many-arguments
 
+import numpy as np
+
 """
 Class for managing integrating spheres.
 
@@ -13,21 +15,42 @@ s"""
 
 class Sphere():
     """Container class for an integrating sphere."""
-    def __init__(self, d, d_sphere, d_sample, d_entrance=0, d_detector=0, r_detector=0, r_wall=1):
+    def __init__(self, d_sphere, d_sample, d_entrance=0, d_detector=0, r_detector=0, r_wall=1):
         """Object initialization."""
 
-        self._d = d
         self._d_sphere = d_sphere
         self._d_sample = d_sample
         self._d_entrance = d_entrance
         self._d_detector = d_detector
 
-        self.a_sample = (self._d_sample/self._d_sphere)**2
-        self.a_detector = (self._d_detector/self._d_sphere)**2
-        self.a_entrance = (self._d_entrance/self._d_sphere)**2
-        self.a_walls = 1 - self.a_sample - self.a_entrance - self.a_detector
+        self.a_sample = self.relative_cap_area(d_sample)
+        self.a_detector = self.relative_cap_area(d_detector)
+        self.a_entrance = self.relative_cap_area(d_entrance)
+        self.a_wall = 1 - self.a_sample - self.a_entrance - self.a_detector
         self.r_detector = r_detector
-        self.r_walls = r_wall
+        self.r_wall = r_wall
+
+    def spherical_cap_area(self, d_port):
+        """Calculate area of spherical cap."""
+        R = self.d_sphere/2
+        r = d_port/2
+        h = R - np.sqrt(R**2-r**2)
+        return 2*np.pi*R*h
+   
+    def approx_relative_cap_area(self, d_port):
+        """Calculate approx relative area of spherical cap."""
+        R = self.d_sphere/2
+        r = d_port/2
+        return r**2/(4*R**2)
+
+    def relative_cap_area(self, d_port):
+        """Calculate relative area of spherical cap."""
+#        R = self.d_sphere/2
+#        r = d_port/2
+#        h = R - np.sqrt(R**2-r**2)
+#        return 2*np.pi*R*h / (4*np.pi*R**2)
+        h = (self.d_sphere - np.sqrt(self.d_sphere**2-d_port**2))/2
+        return h/self.d_sphere
 
     def __str__(self):
         """Return basic details as a string for printing."""
@@ -38,11 +61,11 @@ class Sphere():
         s += "       entrance = %.1f mm\n" % self._d_entrance
         s += "       detector = %.1f mm\n" % self._d_detector
         s += "Diffuse reflectivities \n"
-        s += "           wall = %.1f%%\n" % self.r_walls
+        s += "           wall = %.1f%%\n" % self.r_wall
         s += "       detector = %.1f%%\n" % self.r_detector
         return s
 
-    def gain(self, URU, r_wall=None):
+    def gain(self, r_wall=None):
         """
         Determine the gain for this integrating sphere.
 
@@ -60,15 +83,56 @@ class Sphere():
             gain on detector caused by multiple bounces in sphere
         """
         if r_wall is None:
-            r_wall = self.r_walls
+            r_wall = self.r_wall
 
         tmp = self.a_detector*self.r_detector + self.a_sample*URU
-        tmp = r_wall*(self.a_walls + (1 - self.a_entrance) * tmp)
+        tmp = r_wall*(self.a_wall + (1 - self.a_entrance) * tmp)
         if tmp == 1.0:
             G = r_wall
         else:
             G = r_wall * (1.0 + tmp / (1.0 - tmp))
         return G
+
+    def multiplier(self, r_wall=None):
+        """
+        Determine the sphere multiplier.
+
+        As defined by LabSphere, "Technical Guide: integrating Sphere Theory
+        and application" using equation 13
+        
+        This assumes the reflectance of all ports is zero.
+
+        Args:
+            r_wall: fractional wall reflectivity
+        Returns:
+            sphere muliplier
+        """
+        if r_wall is None:
+            r_wall = self.r_wall
+
+        return r_wall/(1-r_wall*self.a_wall)
+
+    def average_reflectance(self, R, r_wall=None):
+        """
+        Determine the average reflectance of a sphere.
+
+        As defined by LabSphere, "Technical Guide: integrating Sphere Theory
+        and application" using equation 14
+
+        Args:
+            R: the initial reflectance of the sample
+            r_wall: fractional wall reflectivity
+        Returns:
+            average reflectance of the sphere
+        """
+        if r_wall is None:
+            r_wall = self.r_wall
+
+        denom = 1
+        denom -= r_wall*self.a_wall
+        denom -= self.r_detector * self.a_detector
+        denom -= self.r_sample * R
+        return R/denom
 
     @property
     def d_sphere(self):
@@ -79,10 +143,10 @@ class Sphere():
     def d_sphere(self, value):
         """When size is changed ratios become invalid."""
         self._d_sphere = value
-        self.a_sample = (self._d_sample/self._d_sphere)**2
-        self.a_detector = (self._d_detector/self._d_sphere)**2
-        self.a_entrance = (self._d_entrance/self._d_sphere)**2
-        self.a_walls = 1 - self.a_sample - self.a_entrance - self.a_detector
+        self.a_sample = self.relative_cap_area(self._d_sample)
+        self.a_detector = self.relative_cap_area(self._d_detector)
+        self.a_entrance = self.relative_cap_area(self._d_entrance)
+        self.a_wall = 1 - self.a_sample - self.a_entrance - self.a_detector
 
     @property
     def d_sample(self):
@@ -92,9 +156,9 @@ class Sphere():
     @d_sample.setter
     def d_sample(self, value):
         """When size is changed ratios become invalid."""
-        self._d_sphere = value
-        self.a_sample = (self._d_sample/self._d_sphere)**2
-        self.a_walls = 1 - self.a_sample - self.a_entrance - self.a_detector
+        self._d_sample = value
+        self.a_sample = self.relative_cap_area(value)
+        self.a_wall = 1 - self.a_sample - self.a_entrance - self.a_detector
 
     @property
     def d_entrance(self):
@@ -105,8 +169,8 @@ class Sphere():
     def d_entrance(self, value):
         """When size is changed ratios become invalid."""
         self._d_entrance = value
-        self.a_entrance = (self._d_entrance/self._d_sphere)**2
-        self.a_walls = 1 - self.a_sample - self.a_entrance - self.a_detector
+        self.a_entrance = self.relative_cap_area(value)
+        self.a_wall = 1 - self.a_sample - self.a_entrance - self.a_detector
 
     @property
     def d_detector(self):
@@ -117,8 +181,8 @@ class Sphere():
     def d_detector(self, value):
         """When size is changed ratios become invalid."""
         self._d_detector = value
-        self.a_detector = (self._d_detector/self._d_sphere)**2
-        self.a_walls = 1 - self.a_sample - self.a_entrance - self.a_detector
+        self.a_detector = self.relative_cap_area(value)
+        self.a_wall = 1 - self.a_sample - self.a_entrance - self.a_detector
 
 def Gain_11(RS, TS, URU, tdiffuse):
     """
@@ -135,7 +199,7 @@ def Gain_11(RS, TS, URU, tdiffuse):
     GP= TS.gain(URU)
 
     areas = RS.a_sample * TS.a_sample * (1-RS.a_entrance) * (1-TS.a_entrance)
-    G11 = G / (1 - areas * RS.r_walls * TS.r_walls * G * GP * tdiffuse**2)
+    G11 = G / (1 - areas * RS.r_wall * TS.r_wall * G * GP * tdiffuse**2)
     return G11
 
 def Gain_22(RS, TS, URU, tdiffuse):
@@ -154,7 +218,7 @@ def Gain_22(RS, TS, URU, tdiffuse):
     GP= TS.gain(URU)
 
     areas = RS.a_sample * TS.a_sample * (1-RS.a_entrance) * (1-TS.a_entrance)
-    G22 = GP / (1 - areas * RS.r_walls * TS.r_walls * G * GP * tdiffuse**2)
+    G22 = GP / (1 - areas * RS.r_wall * TS.r_wall * G * GP * tdiffuse**2)
     return G22
 
 def Two_Sphere_R(RS, TS, UR1, URU, UT1, UTU, f=0):
@@ -179,10 +243,10 @@ def Two_Sphere_R(RS, TS, UR1, URU, UT1, UTU, f=0):
     GP= TS.gain(URU)
     G11 = Gain_11(RS, TS, URU, UTU)
 
-    x = RS.a_detector*(1-RS.a_entrance)*RS.r_walls*G11
+    x = RS.a_detector*(1-RS.a_entrance)*RS.r_wall*G11
     p1 = (1-f)*UR1
-    p2 = RS.r_walls*f
-    p3 = (1-f)*TS.a_sample*(1-TS.a_entrance)*TS.r_walls*UT1*UTU*GP
+    p2 = RS.r_wall*f
+    p3 = (1-f)*TS.a_sample*(1-TS.a_entrance)*TS.r_wall*UT1*UTU*GP
     return x * (p1 + p2 + p3)
 
 
@@ -204,6 +268,6 @@ def Two_Sphere_T(RS, TS, UR1, URU, UT1, UTU, f=0):
     G= RS.gain(URU)
     G22 = Gain_11(RS, TS, URU, UTU)
 
-    x = TS.a_detector*(1-TS.a_entrance)*TS.r_walls*G22
-    x *= (1-f)*UT1+(1-RS.a_entrance)*RS.r_walls*RS.a_sample*UTU*(f*RS.r_walls+(1-f)*UR1)*G
+    x = TS.a_detector*(1-TS.a_entrance)*TS.r_wall*G22
+    x *= (1-f)*UT1+(1-RS.a_entrance)*RS.r_wall*RS.a_sample*UTU*(f*RS.r_wall+(1-f)*UR1)*G
     return x
