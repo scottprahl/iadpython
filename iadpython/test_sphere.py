787ff1e67d9a832f59b9bfa3b66e22042cb86f42
Scott Prahl <scott@prahls.com>
2021-06-09 18:30:20 -0700

    fix cap test

--- iadpython/test_sphere.py
+++ iadpython/test_sphere.py
@@ -35,6 +35,19 @@ class A_nothing_sandwich(unittest.TestCase):
         np.testing.assert_allclose(s.d_entrance, 10, atol=1e-5)
         np.testing.assert_allclose(s.d_detector, 5, atol=1e-5)
 
+    def test_03_cap(self):
+        """Spherical cap calculations."""
+        R = 100
+        s = iadpython.sphere.Sphere(2*R,5)
+        r = 10
+        acap = s.cap_area(2*r)/(4*np.pi*R**2)
+        a_cap = s.relative_cap_area(2*r)
+        np.testing.assert_allclose(acap, a_cap, atol=1e-5)
+        
+        acap1 = np.pi*r**2/(4*np.pi*R**2)
+        a_cap1 = s.approx_relative_cap_area(20)
+        np.testing.assert_allclose(acap1, a_cap1, atol=1e-5)
+
 
 
 if __name__ == '__main__':
