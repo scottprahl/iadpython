d463968368a43c13b25085bfbc29e521db505e87
Scott Prahl <scott@prahls.com>
2021-12-09 12:15:31 -0800

    add default b and default g tests

--- tests/test_iad.py
+++ tests/test_iad.py
@@ -25,7 +25,7 @@ class IADTest(unittest.TestCase):
         a, b, g = exp.invert()
         self.assertAlmostEqual(a,0.3,delta=1e-4)
         self.assertAlmostEqual(b,np.inf)
-        self.assertAlmostEqual(g,0,delta=1e-4)
+        self.assertAlmostEqual(g,0)
 
     def test_03(self):
         """Matched slab with albedo=0."""
@@ -33,15 +33,49 @@ class IADTest(unittest.TestCase):
         a, b, g = exp.invert()
         self.assertAlmostEqual(a,0.0,delta=1e-4)
         self.assertAlmostEqual(b,np.inf)
-        self.assertAlmostEqual(g,0,delta=1e-4)
+        self.assertAlmostEqual(g,0)
 
-    def test_04(self):
-        """Matched slab with albedo=1."""
-        exp = iadpython.Experiment(r=1)
+#     def test_04(self):
+#         """Matched slab with albedo=1."""
+#         exp = iadpython.Experiment(r=1)
+#         a, b, g = exp.invert()
+#         self.assertAlmostEqual(a,1.0,delta=1e-4)
+#         self.assertAlmostEqual(b,np.inf)
+#         self.assertAlmostEqual(g,0,delta=1e-4)
+
+    def test_05(self):
+        """Matched slab with g=0.9."""
+        exp = iadpython.Experiment(r=0.13865, default_g=0.9)
         a, b, g = exp.invert()
-        self.assertAlmostEqual(a,1.0,delta=1e-4)
+        self.assertAlmostEqual(a,0.95,delta=1e-3)
         self.assertAlmostEqual(b,np.inf)
-        self.assertAlmostEqual(g,0,delta=1e-4)
+        self.assertAlmostEqual(g,0.9)
+
+    def test_06(self):
+        """Matched slab with b=1."""
+        exp = iadpython.Experiment(r=0.30172, default_b=1)
+        a, b, g = exp.invert()
+        self.assertAlmostEqual(a,0.95,delta=1e-3)
+        self.assertAlmostEqual(b,1)
+        self.assertAlmostEqual(g,0.0)
+
+    def test_07(self):
+        """Mismatched slab with albedo=0.95."""
+        s = iadpython.Sample(n=1.4)
+        exp = iadpython.Experiment(r= 0.38697, sample=s)
+        a, b, g = exp.invert()
+        self.assertAlmostEqual(a,0.95,delta=2e-2)
+        self.assertAlmostEqual(b,np.inf)
+        self.assertAlmostEqual(g,0)
+
+    def test_08(self):
+        """Mismatched slab glass slide and albedo=0.95."""
+        s = iadpython.Sample(n=1.4, n_above=1.5, n_below=1.5)
+        exp = iadpython.Experiment(r= 0.39152, sample=s)
+        a, b, g = exp.invert()
+        self.assertAlmostEqual(a,0.95,delta=2e-2)
+        self.assertAlmostEqual(b,np.inf)
+        self.assertAlmostEqual(g,0)
 
 if __name__ == '__main__':
     unittest.main()
