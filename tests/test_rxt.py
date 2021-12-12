b7f33b1a3bf92cf2fc00a714c0c834aa2a229c2a
Scott Prahl <scott@prahls.com>
2021-12-12 10:41:18 -0800

    add more test files

--- tests/test_rxt.py
+++ tests/test_rxt.py
@@ -13,8 +13,8 @@ class TestRXT(unittest.TestCase):
     """RXT files data."""
 
     def test_rxt_01(self):
-        """Verify that subject 1 is read correctly."""
-        exp = iadpython.read_rxt('./tests/basic-A.rxt')
+        """Verify m_r measurements read correctly."""
+        exp = iadpython.read_rxt('./tests/data/basic-A.rxt')
         self.assertAlmostEqual(exp.m_r[0], 0.299262, delta=1e-5)
         self.assertAlmostEqual(exp.m_r[-1], 0.09662, delta=1e-5)
         self.assertIsNone(exp.m_t)
@@ -22,8 +22,8 @@ class TestRXT(unittest.TestCase):
         self.assertIsNone(exp.lambda0)
 
     def test_rxt_02(self):
-        """Verify that subject 1 is read correctly."""
-        exp = iadpython.read_rxt('./tests/basic-B.rxt')
+        """Verify m_r and m_t measurements read correctly."""
+        exp = iadpython.read_rxt('./tests/data/basic-B.rxt')
         self.assertAlmostEqual(exp.m_r[0], 0.51485, delta=1e-5)
         self.assertAlmostEqual(exp.m_t[0], 0.19596, delta=1e-5)
         self.assertAlmostEqual(exp.m_r[-1], 0.44875, delta=1e-5)
@@ -32,8 +32,8 @@ class TestRXT(unittest.TestCase):
         self.assertIsNone(exp.lambda0)
 
     def test_rxt_03(self):
-        """Verify that subject 1 is read correctly."""
-        exp = iadpython.read_rxt('./tests/basic-C.rxt')
+        """Verify m_r, m_t, and m_u measurements read correctly."""
+        exp = iadpython.read_rxt('./tests/data/basic-C.rxt')
         self.assertAlmostEqual(exp.m_r[0], 0.18744, delta=1e-5)
         self.assertAlmostEqual(exp.m_t[0], 0.57620, delta=1e-5)
         self.assertAlmostEqual(exp.m_u[0], 0.00560, delta=1e-5)
@@ -42,6 +42,26 @@ class TestRXT(unittest.TestCase):
         self.assertAlmostEqual(exp.m_u[-1], 6.341e-12, delta=1e-14)
         self.assertIsNone(exp.lambda0)
 
+    def test_rxt_04(self):
+        """Verify lambda, m_r, m_t read correctly."""
+        exp = iadpython.read_rxt('./tests/data/sample-A.rxt')
+        self.assertAlmostEqual(exp.lambda0[0], 800)
+        self.assertAlmostEqual(exp.m_r[0], 0.16830, delta=1e-5)
+        self.assertAlmostEqual(exp.m_t[0], 0.24974, delta=1e-5)
+        self.assertAlmostEqual(exp.lambda0[-1], 1000)
+        self.assertAlmostEqual(exp.m_r[-1], 0.28689, delta=1e-5)
+        self.assertAlmostEqual(exp.m_t[-1], 0.42759, delta=1e-5)
+        self.assertIsNone(exp.m_u)
+
+    def test_rxt_05(self):
+        """Verify lambda, m_r read correctly."""
+        exp = iadpython.read_rxt('./tests/data/sample-B.rxt')
+        self.assertAlmostEqual(exp.lambda0[0], 800)
+        self.assertAlmostEqual(exp.m_r[0], 0.16830, delta=1e-5)
+        self.assertAlmostEqual(exp.lambda0[-1], 1000)
+        self.assertAlmostEqual(exp.m_r[-1], 0.28689, delta=1e-5)
+        self.assertIsNone(exp.m_t)
+        self.assertIsNone(exp.m_u)
 
 if __name__ == '__main__':
     unittest.main()
