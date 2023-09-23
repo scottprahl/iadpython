bbaf9d333963c90318ea8867f611361f81e6e777
Scott Prahl <scott@prahls.com>
2023-09-23 11:21:15 -0700

    remove .git from url

--- iadpython/__init__.py
+++ iadpython/__init__.py
@@ -37,7 +37,7 @@ __author__ = 'Scott Prahl'
 __email__ = 'scott.prahl@oit.edu'
 __copyright__ = '2018-23, Scott Prahl'
 __license__ = 'MIT'
-__url__ = 'https://github.com/scottprahl/iadpython.git'
+__url__ = 'https://github.com/scottprahl/iadpython'
 
 from .constants import *
 from .fresnel import *
